1f46ebb648e160d0085b48b03b16be1f0e72e561
yayee-code <yayeebankz@gmail.com>
2023-10-11 22:17:47 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,17 +9,16 @@ void print_alphabet_x10(void)
 {
 
 char ch;
-	int i;
-	i = 0;
-
-	while (i < 10)
-	{
-		for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		_putchar(ch);
-	}
-		_putchar('\n');
-		i++;
-	}
+int i;
+i = 0;
+while (i < 10)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+i++;
+}
 
 }
