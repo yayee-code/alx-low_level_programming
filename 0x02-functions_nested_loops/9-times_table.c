a0bc6eca75b895575a0f545f983bbf55e48c7fdf
yayee-code <yayeebankz@gmail.com>
2023-10-12 09:43:07 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,11 +22,11 @@ void times_table(void)
 	_putchar(44);
 	_putchar(32);
 	_putchar(d + '0');
-	_putchar(u +'0');
+	_putchar(u + '0');
 	}
 	else
 	{
-	if (y !=0)
+	if (y ! = 0)
 	{
 	_putchar(44);
 	_putchar(32);
