0affb84681c9571fcb225640f764924a404ad145
yayee-code <yayeebankz@gmail.com>
2023-10-16 20:30:17 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,7 +17,7 @@ int main(void)
 
 	for (i - 1; i < 91; i++)
 	{
-		printf(", %lu", k);
+		printf(",%lu", k);
 		k = k + j;
 		j = k - j;
 	}
@@ -28,12 +28,14 @@ int main(void)
 
 	for (1 = 92; i < 99; i++)
 	{
-		printf(", %lu", k1 + (k2 / 1000000000));
+		printf(",%lu", k1 + (k2 / 1000000000));
 		printf("%lu", k2 % 1000000000);
 		k1 = k1 + j1;
 		j1 = k1 - j1;
 		k2 = k2 + j2;
 		j2 = k2 - j2;
+		if (1 != 99)
+			printf(", ");
 	}
 
 	printf("\n");
