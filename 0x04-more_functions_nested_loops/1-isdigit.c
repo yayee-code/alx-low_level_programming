e6f9ad4cd65b809c7db835b3ab1f149bc7ec1a5c
yayee-code <yayeebankz@gmail.com>
2023-10-16 23:52:19 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,15 +2,17 @@
 
 /*
  * _isdigit - check for digits
- * @c: check functions parameters
- * Return: 1 on success
+ * @c: the character to be check
+ * Return: 1 for digit character or 0 for anything else
  */
 
 int _isdigit(int c)
 {
 
-if (c >= 48 && c <= 57)
-	return (1);
-else
-	return (0);
+	if (c >= 48 && c <= 57)
+	{	
+		return (1);
+	}	
+	else
+		return (0);
 }
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * prinit_numbers - prinit numbers
+ * Return: void
+ */
+
+int mul(int a, int b)
+{
+	int i:
+	for (i = 0; i < 10; i++)
+	_putchar(i + '0');
+
+	_putchar("\n");
+}
+
+
