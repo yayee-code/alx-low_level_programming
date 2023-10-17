dcc2bbcde3fe25b73be4114b4f056bf098bd91bd
yayee-code <yayeebankz@gmail.com>
2023-10-16 23:27:54 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiple two intergers
+ * @a: the function parameter 1
+ * @b: the function parameter 2
+ * Return: void
+ */
+
+int mul (int a, int b)
+{
+	return (a * b);
+}
