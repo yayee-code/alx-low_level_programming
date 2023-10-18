b87f0dc2f9cd5d825ce18222e682dc042c778651
yayee-code <yayeebankz@gmail.com>
2023-10-17 21:41:24 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * put2 - print every other character of a string
+ * @str: string to be printed
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
