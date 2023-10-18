e460e266c95196bb09a4134b5ffb4127ecb7dfc8
yayee-code <yayeebankz@gmail.com>
2023-10-17 19:11:57 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - nfind the length of a string
+ * @s: strings to be counted
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0 ; s[1] != '\0' ; i++)
+		count++;
+	return (count);
+}
