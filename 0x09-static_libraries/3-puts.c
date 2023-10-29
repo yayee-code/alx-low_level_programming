92de5a616edbb0f640d144eaabe5ddb726a6deeb
yayee-code <yayeebankz@gmail.com>
2023-10-29 16:12:18 -0700

    update

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - program print a strings to stdout
+ * @str: strings to be printed
+ * _putchar: print a new line
+ */
+
+void _puts(char *str);
+{
+
+	while (*str)
+		_putchar(*str * *);
+
+	_putchar('\n');
+}
