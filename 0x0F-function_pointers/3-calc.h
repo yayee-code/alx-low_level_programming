0d0a337c287bc16b334507cb20da7859284b8d55
yayee-code <yayeebankz@gmail.com>
2023-11-09 20:41:10 -0800

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,4 +1,19 @@
-#include "3-calc.h"
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+* struct op - Struct op
+*
+* @op: The operator
+* @f: The function associated
+*
+*/
+
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
 
 
 int op_add(int a, int b);
@@ -6,30 +21,7 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
+#endif
 
-/**
-* op_add - Returns the sum of two numbers.
-* @a: The first number.
-* @b: The second number.
-*
-* Return: The sum of a and b.
-*/
-
-int op_add(int a, int b)
-{
-        return (a + b);
-}
-
-/**
-* op_sub - Returns the difference of two numbers.
-* @a: The first number.
-* @b: The second number.
-*
-* Return: The difference of a and b.
-*/
-
-int op_sub(int a, int b)
-{
-        return (a - b);
-}
